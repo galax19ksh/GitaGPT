56793a08fa62fabf048766756f45905d4328c457
Galax Kshetrimayum <112553872+galax19ksh@users.noreply.github.com>
2024-11-12 08:29:18 +0530

    Update query_engine.py

--- query_engine.py
+++ query_engine.py
@@ -1,7 +1,4 @@
-# from llm import setup_llm
-
 def create_query_engine(index):
-    # llm = setup_llm()
     return index.as_query_engine(streaming=True) #other parameters like llm= also passable
 
 def create_chat_engine(index):
